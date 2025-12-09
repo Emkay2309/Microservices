11e424e4953a0ffe414b636681cbfe0a2a4b3ef4
Emkay2309 <emkay2309@gmail.com>
2025-12-10 01:21:11 +0530

    Added API Gateway with outh2 and route configurations  and Notifiaction-Service

---
+++ api-gateway/src/main/java/org/project/apigateway/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.project.apigateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange ->
+//                        exchange.pathMatchers("/eureka/**")
+//                                .permitAll()
+//                                .anyExchange()
+//                                .authenticated())
+//                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+//        return serverHttpSecurity.build();
+//    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/**")  // Allow all for testing
+                                .permitAll()
+                                .anyExchange()
+                                .permitAll())
+        // Remove oauth2ResourceServer configuration temporarily
+        ;
+        return serverHttpSecurity.build();
+    }
+}
